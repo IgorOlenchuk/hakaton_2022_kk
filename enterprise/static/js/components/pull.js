1697a30f0103a5ddfb20faa615adc3ae90ae301f
IgorOlenchuk <olenchuk.io@yandex.ru>
2022-08-28 10:02:41 +0300

    final

--- enterprise/static/js/components/pull.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.anues_udokus').addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('.clearfix').classList.toggle('akuvenan');
-    });
--- enterprise/users/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import CreateView
-
-from .forms import CreationForm
-
-
-class SignUp(CreateView):
-    form_class = CreationForm
-    success_url = '/auth/login/'
-    template_name = 'signup.html'
